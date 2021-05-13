094fcb7cf554852bdfcb7cf104c0a240bef50d3f
unknown <qu4dro.sl@gmail.com>
2021-05-13 11:28:11 +0800

    Project structure created

--- app/src/main/java/ru/orlovvv/ort/ui/OrtActivity.kt
+++ app/src/main/java/ru/orlovvv/ort/ui/OrtActivity.kt
@@ -1,9 +1,10 @@
-package ru.orlovvv.ort
+package ru.orlovvv.ort.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.orlovvv.ort.R
 
-class MainActivity : AppCompatActivity() {
+class OrtActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
