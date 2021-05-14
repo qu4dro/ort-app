f92cab36631a077846ac263007da7da622aa376b
unknown <qu4dro.sl@gmail.com>
2021-05-14 14:19:13 +0800

    Updated with db methods

--- app/src/main/java/ru/orlovvv/ort/repository/OrtRepository.kt
+++ app/src/main/java/ru/orlovvv/ort/repository/OrtRepository.kt
@@ -1,9 +1,19 @@
 package ru.orlovvv.ort.repository
 
-class OrtRepository {
+import androidx.lifecycle.LiveData
+import ru.orlovvv.ort.db.OrtDatabase
+import ru.orlovvv.ort.models.LocationPreview
+
+class OrtRepository(private val ortDB: OrtDatabase) {
 
     // network
 
     // database
+    suspend fun insertLocation(locationPreview: LocationPreview) =
+        ortDB.getOrtDao().insertLocation(locationPreview)
 
+    suspend fun deleteLocation(locationPreview: LocationPreview) =
+        ortDB.getOrtDao().deleteLocation(locationPreview)
+
+    fun getAllLocations(): LiveData<List<LocationPreview>> = ortDB.getOrtDao().getAllLocations()
 }
