281de8d4dab41b1033d64e40527cebd708f61068
unknown <qu4dro.sl@gmail.com>
2021-05-14 12:31:13 +0800

    Created adapter for locationPreview

---
+++ app/src/main/java/ru/orlovvv/ort/adapters/LocationAdapter.kt
@@ -0,0 +1,63 @@
+package ru.orlovvv.ort.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.orlovvv.ort.databinding.ItemLocationPreviewBinding
+import ru.orlovvv.ort.models.LocationPreview
+
+class LocationAdapter :
+    ListAdapter<LocationPreview, LocationAdapter.LocationViewHolder>(LocationCallBack()) {
+
+    class LocationViewHolder(private val binding: ItemLocationPreviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(locationPreview: LocationPreview) {
+            binding.location = locationPreview
+            binding.executePendingBindings()
+        }
+    }
+
+    class LocationCallBack : DiffUtil.ItemCallback<LocationPreview>() {
+        override fun areItemsTheSame(oldItem: LocationPreview, newItem: LocationPreview): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: LocationPreview,
+            newItem: LocationPreview
+        ): Boolean {
+            return oldItem._id == newItem._id
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationViewHolder {
+        return LocationViewHolder(
+            ItemLocationPreviewBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: LocationAdapter.LocationViewHolder, position: Int) {
+        val locationPreview = getItem(position)
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.let {
+                it(locationPreview)
+            }
+        }
+
+        holder.bind(locationPreview)
+
+    }
+
+    private var onItemClickListener: ((LocationPreview) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (LocationPreview) -> Unit) {
+        onItemClickListener = listener
+    }
+
+}
