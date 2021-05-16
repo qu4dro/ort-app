013a4c85383482b77f7bb9d8bf996edb18663d6d
unknown <qu4dro.sl@gmail.com>
2021-05-16 17:26:20 +0800

    Added add location fragment, navigation to this fragment and basic code

--- app/src/main/java/ru/orlovvv/ort/ui/OrtActivity.kt
+++ app/src/main/java/ru/orlovvv/ort/ui/OrtActivity.kt
@@ -2,10 +2,13 @@ package ru.orlovvv.ort.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_ort.*
+import ru.orlovvv.ort.R
 import ru.orlovvv.ort.databinding.ActivityOrtBinding
 import ru.orlovvv.ort.db.OrtDatabase
 import ru.orlovvv.ort.repository.OrtRepository
@@ -31,5 +34,32 @@ class OrtActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        setNavigationListeners()
+    }
+
+    private fun setNavigationListeners() {
+        nav_host_fragment.findNavController()
+            .addOnDestinationChangedListener { contoller, destination, arguments ->
+                when (destination.id) {
+                    R.id.mapsFragment -> {
+                        binding.btnAddLocation.show()
+
+                    }
+                    R.id.nearbyLocationsFragment -> {
+                        binding.btnAddLocation.show()
+
+                    }
+                    else -> binding.btnAddLocation.hide()
+                }
+
+            }
+
+        binding.btnAddLocation.setOnClickListener {
+            val currentDestination = nav_host_fragment.findNavController().currentDestination
+            if (currentDestination?.id == R.id.nearbyLocationsFragment) nav_host_fragment.findNavController()
+                .navigate(R.id.action_nearbyLocationsFragment_to_addLocationFragment)
+            if (currentDestination?.id == R.id.mapsFragment) nav_host_fragment.findNavController()
+                .navigate(R.id.action_mapsFragment_to_addLocationFragment)
+        }
     }
 }
