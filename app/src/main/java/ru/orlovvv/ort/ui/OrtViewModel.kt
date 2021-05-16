8e3973488ba0b8d899fad60c226dc4c9a56ef6ef
unknown <qu4dro.sl@gmail.com>
2021-05-16 11:29:41 +0800

    Added basic get neaby locations fun and init block with this

--- app/src/main/java/ru/orlovvv/ort/ui/OrtViewModel.kt
+++ app/src/main/java/ru/orlovvv/ort/ui/OrtViewModel.kt
@@ -1,13 +1,52 @@
 package ru.orlovvv.ort.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import ru.orlovvv.ort.models.LocationPreview
 import ru.orlovvv.ort.repository.OrtRepository
+import ru.orlovvv.ort.util.Resource
+import java.lang.Exception
 
 class OrtViewModel(private val ortRepository: OrtRepository) : ViewModel() {
+
+    private var locationsResponse: List<LocationPreview>? = null
 
     private var _allLocationsPreviews = ortRepository.getAllLocations()
     val allLocationsPreviews
         get() = _allLocationsPreviews
 
+    private val _nearbyLocations: MutableLiveData<Resource<List<LocationPreview>>> = MutableLiveData()
+    val nearbyLocations: LiveData<Resource<List<LocationPreview>>>
+        get() = _nearbyLocations
 
+
+    init {
+        getNearbyLocations()
+    }
+
+    fun getNearbyLocations() = viewModelScope.launch {
+        try {
+            Log.d("123", "LOADING: ")
+            _nearbyLocations.value = Resource.Loading()
+            val response = ortRepository.getNearbyLocations(104.299971, 52.222977, 10000)
+            _nearbyLocations.value = handleTopNewsResponse(response)
+        } catch (e: Exception) {
+//            _nearbyLocations.value = Resource.Success((ArrayList()))
+        }
+
+    }
+
+    private fun handleTopNewsResponse(response: Response<List<LocationPreview>>): Resource<List<LocationPreview>> {
+        if (response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
