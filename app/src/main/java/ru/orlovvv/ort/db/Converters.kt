752a0dffebb0f5f6d994b4427d0af5678f080a89
unknown <qu4dro.sl@gmail.com>
2021-05-16 12:57:34 +0800

    Updated converters, removed brackets when string

--- app/src/main/java/ru/orlovvv/ort/db/Converters.kt
+++ app/src/main/java/ru/orlovvv/ort/db/Converters.kt
@@ -1,18 +1,17 @@
 package ru.orlovvv.ort.db
 
 import androidx.room.TypeConverter
-import ru.orlovvv.ort.models.LocationPreview
 
 class Converters {
 
     @TypeConverter
     fun fromServices(value: List<String>): String? {
-        return value.toString()
+        return value.joinToString()
     }
 
     @TypeConverter
     fun toServices(value: String): List<String>? {
-        return value.split(",").map { it.trim() }
+        return value.split(", ")
     }
 
 }
