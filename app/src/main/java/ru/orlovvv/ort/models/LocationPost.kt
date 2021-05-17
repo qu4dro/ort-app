74e04cc2049041fdc740f86bfbc16f764a6fc3a5
unknown <qu4dro.sl@gmail.com>
2021-05-17 13:45:43 +0800

    Created post data class for location add

---
+++ app/src/main/java/ru/orlovvv/ort/models/LocationPost.kt
@@ -0,0 +1,36 @@
+package ru.orlovvv.ort.models
+
+import com.squareup.moshi.Json
+
+data class LocationPost(
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "address")
+    val address: String,
+    @Json(name = "services")
+    val services: String,
+    @Json(name = "lng")
+    val lng: Double,
+    @Json(name = "lat")
+    val lat: Double,
+    @Json(name = "daysWeekDays")
+    val daysWeekDays: String,
+    @Json(name = "openTimeWeekDays")
+    val openTimeWeekDays: String,
+    @Json(name = "closeTimeWeekDays")
+    val closeTimeWeekDays: String,
+    @Json(name = "isClosedWeekDays")
+    val isClosedWeekDays: Boolean = false,
+    @Json(name = "daysWeekend")
+    val daysWeekend: String = " ",
+    @Json(name = "openTimeWeekend")
+    val openTimeWeekend: String = " ",
+    @Json(name = "closeTimeWeekend")
+    val closeTimeWeekend: String = " ",
+    @Json(name = "isClosedWeekend")
+    val isClosedWeekend: Boolean = false
+
+) {
+    @Json(name = "_id")
+    val _id: String? = null
+}
