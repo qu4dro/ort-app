878a3f48fea2f755c2b488c04fdb747edd9bc2f9
unknown <qu4dro.sl@gmail.com>
2021-05-17 10:50:39 +0800

    Updated add location layout, added add button listener, errors and navigation back press updated

--- app/src/main/java/ru/orlovvv/ort/ui/fragments/AddLocationFragment.kt
+++ app/src/main/java/ru/orlovvv/ort/ui/fragments/AddLocationFragment.kt
@@ -1,10 +1,14 @@
 package ru.orlovvv.ort.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.orlovvv.ort.R
 import ru.orlovvv.ort.databinding.FragmentAddLocationBinding
 import ru.orlovvv.ort.databinding.FragmentLocationInfoBinding
@@ -31,6 +35,29 @@ class AddLocationFragment : Fragment(R.layout.fragment_add_location) {
         }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCreateLocation.setOnClickListener {
+            if (binding.etLocationName.text.toString().isEmpty()) binding.tilLocationName.error =
+                R.string.required.toString()
+            if (binding.etLocationTags.text.toString().isEmpty()) binding.tilLocationTags.error =
+                R.string.required.toString()
+            if (binding.etLocationName.text.toString()
+                    .isNotEmpty() && binding.etLocationTags.text.toString().isNotEmpty()) {
+                findNavController().navigate(R.id.action_addLocationFragment_to_nearbyLocationsFragment)
+            }
+        }
+
+        binding.etLocationName.setOnClickListener {
+            binding.tilLocationName.error = null
+        }
+
+        binding.etLocationTags.setOnClickListener {
+            binding.tilLocationTags.error = null
+        }
     }
 
 }
