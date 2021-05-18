9b3434e99af068225babaa647e828cc79aab71a0
unknown <qu4dro.sl@gmail.com>
2021-05-18 19:58:37 +0800

    Updated layout with buttons and added click listeners

--- app/src/main/java/ru/orlovvv/ort/ui/MenuDialog.kt
+++ app/src/main/java/ru/orlovvv/ort/ui/MenuDialog.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import ru.orlovvv.ort.R
 import ru.orlovvv.ort.databinding.DialogMenuBinding
 
 class MenuDialog : BottomSheetDialogFragment() {
@@ -40,6 +42,21 @@ class MenuDialog : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNearbyLocationItem.setOnClickListener {
+            bottomDialog.dismiss()
+            findNavController().navigate(R.id.action_nearbyLocationsFragment_self)
+        }
+
+        binding.btnSavedLocationItem.setOnClickListener {
+            bottomDialog.dismiss()
+            findNavController().navigate(R.id.action_nearbyLocationsFragment_to_savedLocationsFragment)
+        }
+
+        binding.btnMapsLocationItem.setOnClickListener {
+            bottomDialog.dismiss()
+            findNavController().navigate(R.id.action_nearbyLocationsFragment_to_mapsFragment)
+        }
 
     }
 }
