06829294f112f30f76867073973792234f235ce1
unknown <qu4dro.sl@gmail.com>
2021-05-20 15:45:07 +0800

    Added range formatter

--- app/src/main/java/ru/orlovvv/ort/util/BindingAdapters.kt
+++ app/src/main/java/ru/orlovvv/ort/util/BindingAdapters.kt
@@ -1,29 +1,20 @@
 package ru.orlovvv.ort.util
 
-import android.content.Context
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowInsets
-import android.widget.ImageView
-import android.widget.Spinner
-import android.widget.TextView
-import androidx.annotation.DrawableRes
-import androidx.appcompat.content.res.AppCompatResources.getColorStateList
-import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.core.content.ContextCompat
 import androidx.core.view.updateLayoutParams
 import androidx.databinding.BindingAdapter
-import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.Chip
-import com.google.android.material.elevation.ElevationOverlayProvider
 import ru.orlovvv.ort.R
 import ru.orlovvv.ort.adapters.LocationAdapter
 import ru.orlovvv.ort.adapters.ReviewAdapter
 import ru.orlovvv.ort.models.LocationPreview
 import ru.orlovvv.ort.models.Review
+import java.math.RoundingMode
+
 
 @BindingAdapter("locationsPreviewList")
 fun bindLocationsPreviewRecyclerView(recyclerView: RecyclerView, data: List<LocationPreview>?) {
@@ -50,6 +41,17 @@ fun bindLocationRatingColor(chip: Chip, value: Int) {
             chip.setTextColor(ContextCompat.getColor(chip.context, R.color.black_900))
             chip.text = ":("
         }
+    }
+}
+
+@BindingAdapter("rangeBeautify")
+fun bindRangeBeautiful(chip: Chip, value: Double) {
+    if (value < 1000) {
+        val rounded = value.toBigDecimal().setScale(0, RoundingMode.UP)
+        chip.text = chip.context.getString(R.string.meters, rounded)
+    } else {
+        val rounded = (value / 1000.0).toBigDecimal().setScale(1, RoundingMode.UP)
+        chip.text = chip.context.getString(R.string.kilometers, rounded)
     }
 
 }
