1a3d88efe558d4f661e9f5ca88d85d60889c4aeb
unknown <qu4dro.sl@gmail.com>
2021-05-23 11:45:30 +0800

    Added styles for maps

--- app/src/main/java/ru/orlovvv/ort/ui/fragments/MapsFragment.kt
+++ app/src/main/java/ru/orlovvv/ort/ui/fragments/MapsFragment.kt
@@ -1,27 +1,27 @@
 package ru.orlovvv.ort.ui.fragments
 
 import android.annotation.SuppressLint
+import android.content.res.Resources.NotFoundException
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.MapStyleOptions
 import dagger.hilt.android.AndroidEntryPoint
 import ru.orlovvv.ort.R
 import ru.orlovvv.ort.databinding.FragmentMapsBinding
 import ru.orlovvv.ort.ui.LocationViewModel
 import ru.orlovvv.ort.ui.OrtViewModel
+import timber.log.Timber
 
 
 @AndroidEntryPoint
@@ -58,6 +58,21 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
 
             // For showing a move to my location button
             googleMap!!.isMyLocationEnabled = true
+
+            try {
+                // Customise the styling of the base map using a JSON object defined
+                // in a raw resource file.
+                val success = googleMap!!.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                        context, R.raw.map_style
+                    )
+                )
+                if (!success) {
+                    Timber.d("Style parsing failed.")
+                }
+            } catch (e: NotFoundException) {
+                Timber.d(e, "Can't find style. Error: ")
+            }
 
             // For dropping a marker at a point on the Map
 //            val sydney = LatLng(-34.0, 151.0)
